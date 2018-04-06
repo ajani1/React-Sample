095fd8b7204f862af1296025e6dbd72b0e477d4b
Akash Jani <ajani1@hawk.iit.edu>
2018-04-05 18:12:45 -0700

    hola
    hola

--- src/index.js
+++ src/index.js
@@ -10,6 +10,6 @@ const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <App /> Khan
   </Provider>
   , document.querySelector('.container'));
