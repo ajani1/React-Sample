f0609c5962158b6cce67630fa95d3bdd4fe76021
Akash Jani <ajani1@hawk.iit.edu>
2018-04-08 20:49:00 -0700

    Weather App Complete with Google Map

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,11 +3,13 @@ import axios from 'axios';
 const API_KEY = "64a6aad03e4107e52d50a1ca632411fa";
 const ROOT_URL = "http://api.openweathermap.org/data/2.5/forecast?";
 
-const FETCH_WEATHER = 'FETCH_WEATHER';
+export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
     const url = `${ROOT_URL}q=${city},us&APPID=${API_KEY}`;
     const request = axios.get(url);
+
+    console.log('Request is => ', request);
 
     return {
         type: FETCH_WEATHER,
